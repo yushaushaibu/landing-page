3576fd8c656e0dac81cfb43d435ee2ab4b86d1fc
Yushau Shaibu <yushaushaibu1@gmail.com>
2022-06-23 18:44:50 +0000

    copyright added

--- index.js
+++ index.js
@@ -13,6 +13,7 @@ document.querySelectorAll('.nav-link').forEach(n => n.addEventListener("click", 
 }))
 
 
+
     // background: #e43a15;  /* fallback for old browsers */
     // background: -webkit-linear-gradient(to right, #e65245, #e43a15);  /* Chrome 10-25, Safari 5.1-6 */
     // background: linear-gradient(to right, #e65245, #e43a15); /* W3C, IE 10+/ Edge, Firefox 16+, Chrome 26+, Opera 12+, Safari 7+ */
